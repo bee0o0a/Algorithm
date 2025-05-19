4d12502071bbb8346c1cebb1a7e5ba65c3357916
Bee0o0 <beeeeeeeee0o0@gmail.com>
2025-05-20 06:50:27 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
@@ -1,9 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
-#include <utility>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(void) {
@@ -11,18 +6,17 @@ int main(void) {
 	cin.tie(0);
 
 	int n, y = 0, m = 0;
-
 	cin >> n;
 
 	for (int i = 0; i < n; i++) {
 		int d;
 		cin >> d;
 
-		y += d / 30 + 1;
-		m += d / 60 + 1;
+		y += (d / 30 + 1) * 10;
+		m += (d / 60 + 1) * 15;
 	}
 
-	if (y * 10 > m * 15) cout << "M " << m * 15;
-	else if (y * 10 < m * 15) cout << "Y " << y * 10;
-	else cout << "Y M " << m * 15;
+	if (y > m) cout << "M " << m;
+	else if (y < m) cout << "Y " << y;
+	else cout << "Y M " << m;
 }
