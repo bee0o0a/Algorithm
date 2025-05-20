9f9ca2ba84c22f347f50175fe7feee75ca3364fa
Bee0o0 <beeeeeeeee0o0@gmail.com>
2025-05-21 05:03:56 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int z = 0; z < i; z++) cout << ' ';
+        for (int z = i; z < n; z++) cout << '*';
+        cout << '\n';
+    }
+}
